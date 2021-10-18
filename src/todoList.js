37c82b2cfb11d8f9c948187b20f415d4f1be70a5
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2021-10-18 20:56:09 +0200

    display tasks

--- src/todoList.js
+++ src/todoList.js
@@ -1,6 +1,35 @@
 var tasks = [];
 var date, time, hour, minute;
 
+function displayTasks() {
+  const todoElements = document.querySelector("#todoElements");
+
+  const ulExists = document.querySelector("ul");
+  const createUl = document.createElement("ul");
+  createUl.setAttribute("id", "elementsList");
+
+  if (!ulExists) {
+    todoElements.appendChild(createUl);
+  }
+
+  tasks.map(createTaskList(tasks.name, tasks.time));
+}
+
+// Naprawić funkcjonalność
+function createTaskList(arrayName, arrayTime) {
+  console.log(arrayName);
+  const idKey = arrayName;
+  const elementsList = document.querySelector("#elementsList");
+  const createLi = document.createElement("li");
+  createLi.setAttribute("id", idKey);
+  const createNameContent = document.createTextNode(arrayName);
+  const createTimeContent = document.createTextNode(arrayTime);
+
+  elementsList.appendChild(createLi);
+  document.getElementById(arrayName).appendChild(createNameContent);
+  document.getElementById(arrayName).appendChild(createTimeContent);
+}
+
 function addTask() {
   const inputValue = document.querySelector("#newTask");
   date = new Date();
@@ -22,6 +51,8 @@ function addTask() {
 
   //Test
   console.log(tasks);
+
+  displayTasks();
 }
 
 function deleteTask() {}
