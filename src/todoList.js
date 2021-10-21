b33d9fac5064ec1b0a387bacd726b865d6c8a5bc
xReVaNx <kamil.p.rutyna@gmail.com>
2021-10-21 10:00:48 +0200

    Task modify functionality

--- src/todoList.js
+++ src/todoList.js
@@ -26,45 +26,67 @@ function displayTasks() {
   }
 
   for (let i = 0; i < todoArray.length; i++) {
+    // Creating <li>
     const createLi = document.createElement("li");
     const idKey = "task" + i.toString();
-
-    const nameDiv = document.createElement("div");
-    nameDiv.setAttribute("class", "nameDiv");
-    nameDiv.setAttribute("id", "nameDiv" + i.toString());
-    nameDiv.setAttribute("onclick", "isDone(" + i + ")");
-    const optionDiv = document.createElement("div");
-    optionDiv.setAttribute("class", "optionDiv");
-    optionDiv.setAttribute("id", "optionDiv" + i.toString());
-    optionDiv.setAttribute("onclick", "deleteTask(" + i + ")");
-
     createLi.setAttribute("id", idKey);
     elementsList.appendChild(createLi);
-
-    const textValue = document.createTextNode(todoArray[i].name);
     const elementLi = document.getElementById(idKey);
 
+    // Creating <nameDiv>
+    const createNameDiv = document.createElement("div");
+    createNameDiv.setAttribute("class", "nameDiv");
+    createNameDiv.setAttribute("id", "nameDiv" + i.toString());
     nameIdKey = "nameDiv" + i.toString();
-    optionIdKey = "optionDiv" + i.toString();
+    createNameDiv.setAttribute("id", nameIdKey);
+    elementLi.appendChild(createNameDiv);
 
-    nameDiv.setAttribute("id", nameIdKey);
+    // Creating <optionDiv>
+    const createOptionDiv = document.createElement("div");
+    createOptionDiv.setAttribute("class", "optionDiv");
+    createOptionDiv.setAttribute("id", "optionDiv" + i.toString());
+    optionIdKey = "optionDiv" + i.toString();
+    createOptionDiv.setAttribute("id", optionIdKey);
+    elementLi.appendChild(createOptionDiv);
+    const optionDiv = document.getElementById(optionIdKey);
+
+    // Creating option buttons
+    // Creating checkDiv
+    const createCheckDiv = document.createElement("div");
+    createCheckDiv.setAttribute("class", "checkDiv");
+    createCheckDiv.setAttribute("id", "checkDiv" + i);
+    createCheckDiv.setAttribute("onclick", "isDone(" + i + ")");
+    optionDiv.appendChild(createCheckDiv);
+    document.getElementById("checkDiv" + i).innerHTML =
+      "<i class='fas fa-check-square'></i>";
+
+    // Creating modifyDiv
+    const createModifyDiv = document.createElement("div");
+    createModifyDiv.setAttribute("class", "modifyDiv");
+    createModifyDiv.setAttribute("id", "modifyDiv" + i);
+    createModifyDiv.setAttribute("onclick", "modifyTask(" + i + ")");
+    optionDiv.appendChild(createModifyDiv);
+    document.getElementById("modifyDiv" + i).innerHTML =
+      "<i class='fas fa-pencil-alt'></i>";
+
+    // Creating trashDiv
+    const createTrashDiv = document.createElement("div");
+    createTrashDiv.setAttribute("class", "trashDiv");
+    createTrashDiv.setAttribute("id", "trashDiv" + i);
+    createTrashDiv.setAttribute("onclick", "deleteTask(" + i + ")");
+    optionDiv.appendChild(createTrashDiv);
+    document.getElementById("trashDiv" + i).innerHTML =
+      "<i class='fas fa-trash-alt'></i>";
+
+    // Adding options
+    const textValue = document.createTextNode(todoArray[i].name);
+    document.getElementById(nameIdKey).appendChild(textValue);
 
     // isDone
     if (todoArray[i].done === true) {
-      nameDiv.classList.add("done");
+      createNameDiv.classList.add("done");
     }
-
-    optionDiv.setAttribute("id", optionIdKey);
-
-    elementLi.appendChild(nameDiv);
-    elementLi.appendChild(optionDiv);
-
-    document.getElementById(nameIdKey).appendChild(textValue);
-
-    document.getElementById("optionDiv" + i).innerHTML =
-      "<i class='fas fa-trash-alt'></i>";
   }
-  console.log(todoArray);
 }
 
 // Function that add tasks to the list
@@ -86,6 +108,36 @@ function deleteTask(ind) {
   let todo = localStorage.getItem("todo");
   todoArray = JSON.parse(todo);
   todoArray.splice(ind, 1);
+  localStorage.setItem("todo", JSON.stringify(todoArray));
+  displayTasks();
+}
+
+function modifyTask(ind) {
+  let todo = localStorage.getItem("todo");
+  todoArray = JSON.parse(todo);
+
+  const nameDiv = document.getElementById("nameDiv" + ind);
+  const createModifyInput = document.createElement("input");
+  createModifyInput.setAttribute("class", "modifyInput");
+  createModifyInput.setAttribute("id", "modifyInput" + ind);
+  createModifyInput.setAttribute("value", todoArray[ind].name);
+  nameDiv.innerHTML = "";
+  nameDiv.appendChild(createModifyInput);
+  const createSubmitBtn = document.createElement("button");
+  createSubmitBtn.setAttribute("id", "modify-Btn" + ind);
+  createSubmitBtn.setAttribute("class", "modify-Btn");
+  createSubmitBtn.setAttribute("onclick", "submitModify(" + ind + ")");
+  nameDiv.appendChild(createSubmitBtn);
+  document.getElementById("modify-Btn" + ind).innerHTML =
+    "<i class='fas fa-check'></i>";
+}
+
+function submitModify(ind) {
+  let todo = localStorage.getItem("todo");
+  todoArray = JSON.parse(todo);
+
+  let text = document.getElementById("modifyInput" + ind).value;
+  todoArray[ind].name = text;
   localStorage.setItem("todo", JSON.stringify(todoArray));
   displayTasks();
 }
